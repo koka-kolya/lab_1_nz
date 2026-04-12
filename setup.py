f72d0ad4e4b65b6be5a62995fcceff892995fe99
koka-kolya <nikolay.zapolskikh@gmail.com>
2026-04-12 17:20:37 +0300

    Initial commit: created pkg_lab1_nz for ROS 2 Jazzy

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'pkg_lab1_nz'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='koka-kolya',
+    maintainer_email='koka-kolya@todo.todo',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'ros2_ws = pkg_lab1_nz.ros2_ws:main'
+        ],
+    },
+)
