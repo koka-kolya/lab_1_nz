49f29403721f3fd17c3896607aaf2f689eb71a4b
koka-kolya <nikolay.zapolskikh@gmail.com>
2026-04-12 18:30:34 +0300

    First lab is done

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'pkg_lab1_nz'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -13,18 +13,14 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='koka-kolya',
-    maintainer_email='koka-kolya@todo.todo',
+    maintainer='Your Name',
+    maintainer_email='you@example.com',
     description='TODO: Package description',
-    license='Apache-2.0',
-    extras_require={
-        'test': [
-            'pytest',
-        ],
-    },
+    license='TODO: License',
+    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'ros2_ws = pkg_lab1_nz.ros2_ws:main'
+            'first_node = pkg_lab1_nz.first_node:main',   # ← добавляем эту строку
         ],
     },
 )
